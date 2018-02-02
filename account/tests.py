bc27d82973b9c5a1bd3344b911613fa6b69a5a14
Erik.Butler24 <Erik.butler24@gmail.com>
2018-02-01 20:43:33 -0700

    added first test

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from account import models as amod
 
-# Create your tests here.
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        u1 = amod.User()
+        u1.email = 'Homer@simpsons.com'
+        u1.first_name = 'Homer'
+        u1.last_name = 'Simpson'
+        u1.save()
+
+
+
+    def test_user_saved(self):
+        u2 = amod.User.objects.get(email = 'Homer@simpsons.com')
+        self.assertEqual(u2.email, 'Homer@simpsons.com')
+        self.assertEqual(u2.first_name,'Homer')
+        self.assertEqual(u2.last_name,'Simpson')
+
